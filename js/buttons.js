59053c3d8051f5d10dcd9695d4f7eb1cc58a1625
Bellic <159970540+SrBellic@users.noreply.github.com>
2024-04-16 01:01:39 -0400

    update
    Añadidos estilos nuevos para botones y páginas responsivas

---
+++ js/buttons.js
@@ -0,0 +1,12 @@
+function expandirBoton() {
+    document.querySelector('.boton').style.width = '150px';
+    document.querySelector('.boton').style.height = '50px';
+    document.querySelector('.boton').style.borderRadius = '25px';
+  }
+  
+  function contraerBoton() {
+    document.querySelector('.boton').style.width = '50px';
+    document.querySelector('.boton').style.height = '50px';
+    document.querySelector('.boton').style.borderRadius = '50%';
+  }
+  
