3b83a1415ea35af4ccdc6435b399d6ae693ebce0
Bellic <159970540+SrBellic@users.noreply.github.com>
2024-04-22 00:21:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
                                 <h2 class="text-center">Inicia Sesión</h2>
                                 <form action="#" class="my-4">
                                     <label for="user" class="form-label fs-4">Usuario<img src="assets/favicons/system/user32x32.png" class="mb-1 " width="24px"></label>
-                                    <input type="text" class="form-control bg-white shadow my-2" name="user">
+                                    <input type="text" class="form-control fs-4 bg-white shadow my-2" name="user">
                                     <label for="pass" class="form-label fs-4">Contraseña<img src="assets/favicons/system/pass32x32.png" class="mb-1 ms-1" width="24px"></label>
-                                    <input type="password" class="form-control bg-white shadow my-2" name="pass">
+                                    <input type="password" class="form-control fs-4 bg-white shadow my-2" name="pass">
                                     <br>
                                     <div class="d-flex justify-content-center">
                                         <input type="submit" class="button-cyan fw-bold shadow w-50" value="INGRESAR">
@@ -51,30 +51,16 @@
         <footer style="height: 17.2vh;" class="bg-color-gray">
             <hr>
             <div class="container mx-5">
-                <div class="row item-div">
-                    <div class="col-3">
-                        <small>Contactanos:</small>
+                <div class="row">
+                    <div class="col">
+                        <small class="fs-6">Contactanos:</small>
                         <br>
-                        <small><a href="https://api.whatsapp.com/message/ZHW4NUVU4VXVI1?autoload=1&app_absent=0"  class="link-gray">Whatsapp</a></small>
+                        <small class="fs-6"><a href="https://api.whatsapp.com/message/ZHW4NUVU4VXVI1?autoload=1&app_absent=0"  class="link-gray">Whatsapp</a></small>
                         <img src="assets/favicons/whatsapp/whatsapp24x24.png">
                         <br>
-                        <small><a href="#"  class="link-gray">Instagram</a></small>
+                        <small class="fs-6"><a href="https://www.instagram.com/deisanarte?igsh=czhpZzJzcWZnaTI1"  class="link-gray">Instagram</a></small>
                         <img src="assets/favicons/instagram/instagram24x24.svg">
                         <br>
-                        <small><a href="#"  class="link-gray">Correo Electrónico</a></small>
-                        <img src="assets/favicons/email/email24x24.png">
-                    </div>
-                    <div class="col-3">
-                        <div class="mt-4">
-                            <small>Nos ubicamos en:</small>
-                            <br>
-                            <small>Calle Bolívar, C.C. Galerías Baruta, PB, Local 01</small>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="mt-5 text-end">
-                            <h5><a href="#" class="link-gray">Reportar fallo <img src="assets/favicons/system/bug24x24.png" alt=""></a></h5>
-                        </div>
                     </div>
                 </div>
             </div>
